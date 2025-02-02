71634f1055f1436bacc3aee821c64bfd1cc2bdf9
Minh Hieu <hieubuiminh11091997@gmail.com>
2025-02-02 14:19:46 +0700

    update product v2

--- server/src/main/java/com/shop/server/core/admin/billhistory/controller/AdminBillHistoryController.java
+++ server/src/main/java/com/shop/server/core/admin/billhistory/controller/AdminBillHistoryController.java
@@ -1,16 +1,13 @@
 package com.shop.server.core.admin.billhistory.controller;
 
+import com.shop.server.core.admin.billhistory.model.request.AdminCreateHistoryRequest;
 import com.shop.server.core.admin.billhistory.model.request.AdminFindBillHistoryRequest;
 import com.shop.server.core.admin.billhistory.service.AdminBillHistoryService;
 import com.shop.server.infrastructure.constants.module.MappingConstant;
 import com.shop.server.utils.Helper;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(MappingConstant.API_ADMIN_BILL_HISTORY)
