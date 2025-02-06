26890f63f4ce0482ae2b707c80ea7d39508503cd
Thanh292K <hthanh292k@gmail.com>
2025-02-06 20:50:20 +0700

    feat(admin/payment khachhang) API

---
+++ server/src/main/java/com/shop/server/core/admin/ban_hang/repository/AdminPhieuGiamGiaRepository.java
@@ -0,0 +1,91 @@
+package com.shop.server.core.admin.ban_hang.repository;
+
+import com.shop.server.core.admin.ban_hang.model.request.AdminHoaDonKhachHangRequest;
+import com.shop.server.core.admin.ban_hang.model.response.AdminVoucherResponse;
+import com.shop.server.entities.main.PhieuGiamGia;
+import com.shop.server.repositories.PhieuGiamGiaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AdminPhieuGiamGiaRepository extends PhieuGiamGiaRepository {
+    @Query(value = """
+    SELECT  
+            pgg.id AS id,
+            pgg.ma_phieu_giam_gia AS ma,
+            pgg.ten AS ten,
+            pgg.so_luong AS soLuong,
+            pgg.gia_tri_giam AS giaTriGiam,
+            pgg.loai_giam AS loaiGiam,
+            pgg.dieu_kien_giam AS dieuKienGiam,
+            CASE
+             -- Nếu không có tổng đơn hàng, giữ nguyên giá trị gốc
+                    WHEN :#{#request.tongTien} IS NULL THEN
+                        CASE
+                        WHEN pgg.loai_giam = TRUE THEN pgg.gia_tri_giam  -- Giảm tiền mặt
+                                ELSE pgg.gia_tri_giam  -- Giảm phần trăm
+                        END
+                -- Nếu tổng đơn hàng đạt điều kiện, tính toán giảm giá
+                    WHEN :#{#request.tongTien} >= pgg.dieu_kien_giam THEN
+                    CASE
+                            WHEN pgg.loai_giam = FALSE THEN  -- Giảm theo phần trăm
+                                LEAST((pgg.gia_tri_giam / 100) * :#{#request.tongTien}, pgg.giam_toi_da)
+                            ELSE  -- Giảm tiền mặt
+                                LEAST(pgg.gia_tri_giam, pgg.giam_toi_da)
+                    END
+                   ELSE 0
+             END AS giaTriGiam
+             FROM phieu_giam_gia pgg
+                    LEFT JOIN khach_hang_phieu_giam_gia khpgg ON pgg.id = khpgg.id_phieu_giam_gia
+             WHERE pgg.ngay_bat_dau <= UNIX_TIMESTAMP()*1000
+                AND pgg.ngay_ket_thuc >= UNIX_TIMESTAMP()*1000
+                AND pgg.trang_thai = 'ACTIVE'
+                AND (:#{#request.idKhachHang} IS NULL
+                OR khpgg.id_khach_hang = :#{#request.idKhachHang})
+                AND pgg.so_luong > 0
+             ORDER BY giaTriGiam DESC;
+""",nativeQuery = true)
+    Page<AdminVoucherResponse> getPhieuGiamGia(AdminHoaDonKhachHangRequest request, Pageable pageable);
+
+    @Query(value = """
+    SELECT  
+            pgg.id AS id,
+            pgg.ma_phieu_giam_gia AS ma,
+            pgg.ten AS ten,
+            pgg.so_luong AS soLuong,
+            pgg.gia_tri_giam AS giaTriGiam,
+            pgg.loai_giam AS loaiGiam,
+            pgg.dieu_kien_giam AS dieuKienGiam,
+            CASE
+             -- Nếu không có tổng đơn hàng, giữ nguyên giá trị gốc
+                    WHEN :#{#request.tongTien} IS NULL THEN
+                        CASE
+                        WHEN pgg.loai_giam = TRUE THEN pgg.gia_tri_giam  -- Giảm tiền mặt
+                                ELSE pgg.gia_tri_giam  -- Giảm phần trăm
+                        END
+                -- Nếu tổng đơn hàng đạt điều kiện, tính toán giảm giá
+                    WHEN :#{#request.tongTien} >= pgg.dieu_kien_giam THEN
+                    CASE
+                            WHEN pgg.loai_giam = FALSE THEN  -- Giảm theo phần trăm
+                                LEAST((pgg.gia_tri_giam / 100) * :#{#request.tongTien}, pgg.giam_toi_da)
+                            ELSE  -- Giảm tiền mặt
+                                LEAST(pgg.gia_tri_giam, pgg.giam_toi_da)
+                    END
+                   ELSE 0
+             END AS giaTriGiam
+             FROM phieu_giam_gia pgg
+                    LEFT JOIN khach_hang_phieu_giam_gia khpgg ON pgg.id = khpgg.id_phieu_giam_gia
+             WHERE pgg.ngay_bat_dau <= UNIX_TIMESTAMP()*1000
+                AND pgg.ngay_ket_thuc >= UNIX_TIMESTAMP()*1000
+                AND pgg.trang_thai = 'ACTIVE'
+                AND (:#{#request.idKhachHang} IS NULL
+                OR khpgg.id_khach_hang = :#{#request.idKhachHang})
+                AND pgg.so_luong > 0
+                AND pgg.id = (:#{#request.idPhieuGiamGia})
+""",nativeQuery = true)
+    AdminVoucherResponse getPhieuGiamGiaById(AdminHoaDonKhachHangRequest request);
+}
