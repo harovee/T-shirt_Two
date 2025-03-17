dd491fab4571038e25053385455d550869bc6c43
Thanh292K <hthanh292k@gmail.com>
2025-03-18 00:43:16 +0700

    fix config momo

--- server/src/main/java/com/shop/server/ServerApplication.java
+++ server/src/main/java/com/shop/server/ServerApplication.java
@@ -2,14 +2,12 @@ package com.shop.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAsync
-@EnableFeignClients
 public class ServerApplication {
 
     public static void main(String[] args) {
--- server/src/main/java/com/shop/server/core/client/momo/controller/MomoController.java
+++ server/src/main/java/com/shop/server/core/client/momo/controller/MomoController.java
@@ -1,13 +1,14 @@
 package com.shop.server.core.client.momo.controller;
 
-import com.shop.server.core.client.momo.model.MomoRequest;
-import com.shop.server.core.client.momo.model.MomoResponse;
-import com.shop.server.core.client.momo.services.MomoServices;
+import com.shop.server.core.client.momo.model.request.ClientMomoRequest;
+import com.shop.server.core.client.momo.services.IMomoService;
 import com.shop.server.infrastructure.constants.module.MappingConstant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,13 +16,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(MappingConstant.API_CLIENT_PAYMENT)
 public class MomoController {
 
-    private final MomoServices momoServices;
+    private final IMomoService momoServices;
 
-    @PostMapping("/create")
-    ResponseEntity<?> createMomo() {
-        return new ResponseEntity<>(momoServices.createMomo(), HttpStatus.OK);
+    @PostMapping("momo")
+    ResponseEntity<?> createMomo(@RequestBody ClientMomoRequest request) {
+        return new ResponseEntity<>(momoServices.createMomo(request), HttpStatus.OK);
     }
 
-//    @GetMapping("/momo-callback")
-
+    @GetMapping("momo-callback")
+    public String momoCallback(@RequestParam Map<String, String>  request) {
+        Integer resultCode = Integer.valueOf(request.get("resultCode"));
+        if (resultCode == 0) {
+            return "Thanh toán thành công";
+        }
+        else {
+            return "Thanh toán thất bại";
+        }
+    }
 }
