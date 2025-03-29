96ece23c5348fa4a43b4f0c20d4cc45b71494b3c
HaoPq <138428708+harovee@users.noreply.github.com>
2025-03-30 02:59:30 +0700

    FEAT: live chat group

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import {VueQueryDevtools} from "@tanstack/vue-query-devtools";
 import Chatbot from "./Chatbot.vue";
+import LiveChat from "./LiveChat.vue";
 </script>
 
 <template>
   <vue-query-devtools/>
   <router-view/>
   <chatbot/>
+  <live-chat/>
 </template>
 
 <style scoped>
---
+++ server/src/main/java/com/shop/websocket/model/ChatMessage.java
@@ -0,0 +1,20 @@
+package com.shop.websocket.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+    private String roomId;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+}
