437dc31a4355a543b6e0ad3170c62ce79c58074f
Nguyen Thu Thao <166482816+thaocandy-s@users.noreply.github.com>
2025-04-20 13:37:59 +0700

    refactor POS Connect

--- client/src/page/admin/point.of.sale/components/POSProducsInCart.vue
+++ client/src/page/admin/point.of.sale/components/POSProducsInCart.vue
@@ -1,6 +1,6 @@
 <template>
   <h2 class="text-xl font-semibold inline-flex">Giỏ hàng
-    <a-tooltip v-if="isMBConnected" title="Ngắt kết nối" trigger="hover">
+    <!-- <a-tooltip v-if="isMBConnected" title="Ngắt kết nối" trigger="hover">
             <a-button
               class="bg-purple-300 flex justify-between items-center gap-2  ms-4"
               size="large"
@@ -8,7 +8,7 @@
             >
               <v-icon name="bi-link" />
             </a-button>
-   </a-tooltip>
+   </a-tooltip> -->
   </h2>
 
   <div>
