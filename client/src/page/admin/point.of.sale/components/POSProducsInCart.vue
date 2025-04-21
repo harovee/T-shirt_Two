986fcf2ee26855438232de52cf53601bf454d853
Minh Hieu <hieubuiminh11091997@gmail.com>
2025-04-21 20:27:50 +0700

    fix case change price

--- client/src/page/admin/point.of.sale/components/POSProducsInCart.vue
+++ client/src/page/admin/point.of.sale/components/POSProducsInCart.vue
@@ -73,7 +73,8 @@
             <a-input
               type="number"
               v-model:value="record.soLuong"
-              @change="handleQuantityChange(record)"
+              @focus="focusQuantity(record.soLuong)"
+              @blur="handleQuantityChange(record)"
               min="1"
             >
             </a-input>
@@ -153,6 +154,8 @@ import {
   openNotification,
 } from "@/utils/notification.config";
 import { ExclamationCircleOutlined } from "@ant-design/icons-vue";
+import { useCheckQuantityInStock } from "@/infrastructure/services/service/admin/productdetail.action";
+import { checkQuantityRequest } from "@/infrastructure/services/api/admin/product_detail.api";
 
 const props = defineProps<{
   idOrder: string;
@@ -211,7 +214,7 @@ const columns: TableColumnType<DataType>[] = [
 
 //
 
-const { data, isLoading, refetch } = useGetOrderDetails(
+const { data, isLoading, refetch: refetchCart } = useGetOrderDetails(
   props.idOrder?.valueOf(),
   {
     refetchOnWindowFocus: false,
@@ -247,27 +250,68 @@ const dataSource: DataType[] | any = computed(() => {
 const { mutate: updateQuantityOrderDetails } = useUpdateQuantityOrderDetails();
 const { mutate: deleteOrderDetails } = useDeleteCartById();
 
-const handleQuantityChange = (record: any) => {
+const preQuantity = ref(null);
+const tableKey = ref(0);
+
+const focusQuantity = (quantity: number) => {
+  preQuantity.value = quantity;
+}
+
+const params = ref<checkQuantityRequest>({
+  id: null,
+  quantity: null,
+});
+
+const { data: checkQuantityData, refetch: checkQuantityRefetch } =
+  useCheckQuantityInStock(params, {
+    refetchOnWindowFocus: false,
+    keepPreviousData: false,
+    enabled: false,
+  });
+
+const handleQuantityChange = async (record: any) => {
+  params.value.id = record.key;
+  params.value.quantity = record.soLuong;
+
   const payload = {
     idHoaDonChiTiet: record.key,
     soLuongBanSau: record.soLuong,
     soLuongBanTruoc: null,
   };
+
   try {
-    updateQuantityOrderDetails(payload);
-    // successNotiSort("Sửa thành công")
+    // Chờ check số lượng xong trước khi tiếp tục
+    await checkQuantityRefetch();
+    const checkValue = checkQuantityData?.value?.data;
+    
+    if (!checkValue) {
+      warningNotiSort("Số lượng trong kho không đủ!");
+      reloadData();
+    } else {
+      if (payload.soLuongBanSau <= 0) {
+        warningNotiSort("Số lượng không được âm!");
+        reloadData();
+        return;
+      }
+      await updateQuantityOrderDetails(payload);
+    }
   } catch (error: any) {
-    if (error?.response) {
-      openNotification(
-        notificationType.error,
-        error?.response?.data?.message,
-        ""
-      );
-    } else if (error?.errorFields) {
-      openNotification(notificationType.warning, "", "");
+    console.log("⛔ Vào catch...");
+    console.error("🔥 Lỗi từ backend:", error.response?.data || error);
+
+    if (error.response?.status === 400) {
+      warningNotiSort(error.response.data.message);
+    } else {
+      openNotification(notificationType.error, "Lỗi không xác định!", "");
     }
   }
 };
+
+const reloadData = async () => {
+  await refetchCart();
+  tableKey.value++;
+};
+
 const handleDelete = (idHdct: string) => {
   Modal.confirm({
     content: "Bạn chắc chắn muốn xóa sản phẩm này ra khỏi giỏ?",
